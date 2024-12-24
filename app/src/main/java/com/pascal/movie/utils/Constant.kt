d2581229c7c3e25f54f4a6ce4c00b186eb195c3c
pascal <pascaladitia@gmail.com>
2024-12-24 07:53:41 +0700

    - add base_url & apiKey

--- app/src/main/java/com/pascal/movie/utils/Constant.kt
+++ app/src/main/java/com/pascal/movie/utils/Constant.kt
@@ -1,13 +1,13 @@
 package com.pascal.movie.utils
 
 object Constant {
-    const val QUERY = "query"
-    const val HIDE_DIALOG = 0
-
     const val TIMEOUT = 300000L
-    const val AUTHORIZATION = "Authorization"
     const val ERROR_MESSAGE = "Something went wrong, check your connection!"
-    const val ERROR_LOGIN = "Incorrect email or password!"
-    const val BEARER = "Bearer "
     const val FORMAT_DATE = "yyyy-MM-dd"
+
+    const val POSTER_BASE_URL = "https://image.tmdb.org/t/p/"
+    const val YOUTUBE_TN_URL  = "https://img.youtube.com/vi/"
+    const val YOUTUBE_TRAILERS_URL = "https://www.youtube.com/watch?v="
+    const val W185 = "w185"
+    const val W500 = "w500"
 }
