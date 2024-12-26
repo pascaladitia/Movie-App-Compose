e0977e0889b264941915cc5b0378d105c7ec444a
pascal <pascaladitia@gmail.com>
2024-12-26 14:53:21 +0700

    - fix corousel

--- app/src/main/java/com/pascal/movie/ui/screen/home/CustomSlider.kt
+++ app/src/main/java/com/pascal/movie/ui/screen/home/CustomSlider.kt
@@ -23,16 +23,18 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.alpha
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.graphicsLayer
 import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.platform.LocalDensity
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.times
+import androidx.compose.ui.zIndex
 import coil.compose.AsyncImage
 import coil.request.ImageRequest
 import coil.size.Scale
@@ -54,47 +56,50 @@ fun CustomSlider(
     imageCornerRadius: Dp = 16.dp,
     imageHeight: Dp = 250.dp,
 ) {
-
     val pagerState = rememberPagerState(pageCount = { sliderList.size })
     val scope = rememberCoroutineScope()
+    val density = LocalDensity.current
 
     Column(
-        horizontalAlignment = Alignment.CenterHorizontally, verticalArrangement = Arrangement.Center
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center
     ) {
-        Row(
-            modifier = modifier.fillMaxWidth(),
-            verticalAlignment = Alignment.CenterVertically,
-            horizontalArrangement = Arrangement.Center,
+        Box(
+            modifier = modifier.fillMaxWidth()
         ) {
             HorizontalPager(
                 state = pagerState,
                 contentPadding = pagerPaddingValues,
-                modifier = modifier.weight(1f)
+                modifier = modifier
             ) { page ->
                 val pageOffset =
                     (pagerState.currentPage - page) + pagerState.currentPageOffsetFraction
 
                 val scaleFactor = 0.75f + (1f - 0.75f) * (1f - pageOffset.absoluteValue)
+                val transX = with(density) { (pageOffset * 100.dp).toPx() }
+                val zIndex = 1f - pageOffset.absoluteValue
 
-
-                Box(modifier = modifier
-                    .graphicsLayer {
-                        scaleX = scaleFactor
-                        scaleY = scaleFactor
-                    }
-                    .alpha(
-                        scaleFactor.coerceIn(0f, 1f)
-                    )
-                    .padding(10.dp)
-                    .clip(RoundedCornerShape(imageCornerRadius))) {
+                Box(
+                    modifier = Modifier
+                        .graphicsLayer {
+                            scaleX = scaleFactor
+                            scaleY = scaleFactor
+                            translationX = transX
+                        }
+                        .zIndex(zIndex)
+                        .padding(10.dp)
+                        .clip(RoundedCornerShape(imageCornerRadius))
+                ) {
                     AsyncImage(
-                        model = ImageRequest.Builder(LocalContext.current).scale(Scale.FILL)
-                            .crossfade(true).data(sliderList[page]).build(),
+                        model = ImageRequest.Builder(LocalContext.current)
+                            .scale(Scale.FILL)
+                            .crossfade(true)
+                            .data(sliderList[page])
+                            .build(),
                         contentDescription = "Image",
                         contentScale = ContentScale.Crop,
                         placeholder = painterResource(id = R.drawable.no_thumbnail),
-                        modifier = modifier.height(imageHeight)
-//                            .alpha(if (pagerState.currentPage == page) 1f else 0.5f)
+                        modifier = Modifier.height(imageHeight)
                     )
                 }
             }
@@ -108,18 +113,21 @@ fun CustomSlider(
             repeat(sliderList.size) {
                 val color =
                     if (pagerState.currentPage == it) dotsActiveColor else dotsInActiveColor
-                Box(modifier = modifier
-                    .padding(2.dp)
-                    .clip(CircleShape)
-                    .size(dotsSize)
-                    .background(color)
-                    .clickable {
-                        scope.launch {
-                            pagerState.animateScrollToPage(it)
+                Box(
+                    modifier = modifier
+                        .padding(2.dp)
+                        .clip(CircleShape)
+                        .size(dotsSize)
+                        .background(color)
+                        .clickable {
+                            scope.launch {
+                                pagerState.animateScrollToPage(it)
+                            }
                         }
-                    }
                 )
             }
         }
     }
 }
+
+
